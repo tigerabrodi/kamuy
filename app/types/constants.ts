f19c52a3d89297ec4cae5c49cb77739e71140411
tigerabrodi <tigerabrodi@gmail.com>
2022-11-05 15:41:06 +0100

    feat: add constants

---
+++ app/types/constants.ts
@@ -0,0 +1,18 @@
+export const validationStates = {
+  success: 'success',
+  error: 'error',
+} as const
+
+export const ACCESS_TOKEN = 'access_token'
+
+export const SET_COOKIE = 'Set-Cookie'
+
+export const ONE_HOUR = 60 * 60
+
+export const ONE_DAY = ONE_HOUR * 24
+
+export const ONE_WEEK = ONE_DAY * 7
+
+export const ONE_MONTH = ONE_DAY * 30
+
+export const ONE_YEAR = ONE_DAY * 365
