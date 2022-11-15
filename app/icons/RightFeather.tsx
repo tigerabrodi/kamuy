0dd79dc32f78ab358697bc3229e053f96f0925c0
tigerabrodi <tigerabrodi@gmail.com>
2022-11-15 07:38:11 +0100

    chore: add icons

---
+++ app/icons/RightFeather.tsx
@@ -0,0 +1,10 @@
+export function RightFeather() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 20">
+      <path
+        fill="#075E54"
+        d="m.601 19.757 22.7-9.729L.6.3l.011 7.912 13.623 1.816L.612 11.845l-.01 7.912Z"
+      />
+    </svg>
+  )
+}
