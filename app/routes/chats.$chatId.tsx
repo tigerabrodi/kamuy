e7e79369a4c584abc522677e250ed87d52ca6e67
tigerabrodi <tigerabrodi@gmail.com>
2022-11-19 09:12:58 +0100

    feat: implement chatId logic whenever it is newly created

--- app/routes/chats.$chatId.tsx
+++ app/routes/chats.$chatId.tsx
@@ -1,10 +1,12 @@
-import type { DataFunctionArgs, LinksFunction } from '@remix-run/node'
+import type { DataFunctionArgs } from '@remix-run/node'
 import type { Chat } from '~/types/firebase'
 
+import { json } from '@remix-run/node'
 import { Form, Link, useLoaderData } from '@remix-run/react'
 import { z } from 'zod'
+import { zx } from 'zodix'
 
-import styles from './chats.$chatId.css'
+import { IS_NEWLY_CREATED } from './chats'
 
 import { getChatById, getParticipantsWithChatId } from '~/firebase'
 import { DefaultChat, RightFeather, Setting } from '~/icons'
@@ -12,20 +14,20 @@ import { DefaultChat, RightFeather, Setting } from '~/icons'
 const TYPE_A_MESSAGE = 'type a message'
 const ENTER_CHAT_NAME = 'Enter chat name'
 
-export const links: LinksFunction = () => {
-  return [{ rel: 'stylesheet', href: styles }]
-}
-
-export const loader = async ({ params }: DataFunctionArgs) => {
-  const chatId = z.string().parse(params.chatId)
+export const loader = async ({ params, request }: DataFunctionArgs) => {
+  const { chatId } = zx.parseParams(params, { chatId: z.string() })
+  const { isNewlyCreated } = zx.parseQuery(request, {
+    [IS_NEWLY_CREATED]: zx.BoolAsString.optional(),
+  })
 
   const chat = await getChatById(chatId)
   const participants = await getParticipantsWithChatId(chatId)
 
-  return {
+  return json({
     chat,
     participants,
-  }
+    isNewlyCreated,
+  })
 }
 
 function shouldShowDefaultChatImg(chat: Chat) {
@@ -33,7 +35,7 @@ function shouldShowDefaultChatImg(chat: Chat) {
 }
 
 export default function ChatDetail() {
-  const { chat, participants } = useLoaderData<typeof loader>()
+  const { chat, participants, isNewlyCreated } = useLoaderData<typeof loader>()
 
   return (
     <div className="chat">
@@ -51,6 +53,7 @@ export default function ChatDetail() {
           placeholder={ENTER_CHAT_NAME}
           aria-label={ENTER_CHAT_NAME}
           defaultValue={chat.name}
+          autoFocus={isNewlyCreated}
         />
 
         <Link to={`./settings`} aria-label={`Settings of ${chat.name} chat`}>
@@ -80,3 +83,49 @@ export default function ChatDetail() {
     </div>
   )
 }
+
+export function ChatDetailPlaceholder() {
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <DefaultChat />
+
+        <input
+          type="text"
+          id="title"
+          name="title"
+          placeholder={ENTER_CHAT_NAME}
+          aria-label={ENTER_CHAT_NAME}
+          disabled
+        />
+
+        <Link
+          to={`./settings`}
+          aria-label="Settings of pending chat"
+          style={{ pointerEvents: 'none' }}
+        >
+          <Setting />
+        </Link>
+
+        <p>...</p>
+      </div>
+
+      <Form className="chat__form">
+        <input
+          type="text"
+          id="message"
+          name="message"
+          aria-label={TYPE_A_MESSAGE}
+          placeholder={TYPE_A_MESSAGE}
+          disabled
+        />
+
+        <button type="submit" aria-label="Send message" disabled>
+          <RightFeather />
+        </button>
+      </Form>
+    </div>
+  )
+}
+
+// TODO: Handle chat not found
