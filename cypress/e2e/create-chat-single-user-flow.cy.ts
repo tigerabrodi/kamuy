76a08f547287b2836b57c81d84a6cba3edf318e9
tigerabrodi <tigerabrodi@gmail.com>
2022-11-19 09:17:46 +0100

    test: correct current optimistic ui

--- cypress/e2e/create-chat-single-user-flow.cy.ts
+++ cypress/e2e/create-chat-single-user-flow.cy.ts
@@ -22,11 +22,10 @@ it('Should be able to create a chat, write messages and edit the chat.', () => {
   cy.findByRole('button', { name: 'Create new chat' }).click()
 
   // Assert pending UI during submission
-  cy.findByRole('link', { name: 'pending chat' }).should('be.visible')
-
   cy.findByRole('link', { name: `Settings of ${UNTITLED} chat` }).should(
     'not.exist'
   )
+  cy.findByText('...').should('be.visible')
 
   cy.findByLabelText(ENTER_CHAT_NAME).should('be.disabled')
   cy.findByLabelText(TYPE_A_MESSAGE).should('be.disabled')
