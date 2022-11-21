5812419b06505fde22cbc5fd47dee5b96b9adffa
tigerabrodi <tigerabrodi@gmail.com>
2022-11-21 06:48:17 +0100

    feat: implement settings page basic

--- app/routes/chats.$chatId.settings.tsx
+++ app/routes/chats.$chatId.settings.tsx
@@ -1,11 +1,28 @@
 import type { LinksFunction } from '@remix-run/node'
 
+import { Dialog } from '@headlessui/react'
+import { useNavigate } from '@remix-run/react'
+
 import styles from './chats.$chatId.settings.css'
 
 export const links: LinksFunction = () => {
   return [{ rel: 'stylesheet', href: styles }]
 }
 
+const BACK_ROUTE = '..'
+
 export default function Settings() {
-  return <div>gello world</div>
+  const navigate = useNavigate()
+
+  return (
+    <Dialog
+      open={true}
+      onClose={() => navigate(BACK_ROUTE)}
+      className="settings"
+    >
+      <Dialog.Panel className="settings__panel">
+        <Dialog.Title className="sr-only">Deactivate account</Dialog.Title>
+      </Dialog.Panel>
+    </Dialog>
+  )
 }
