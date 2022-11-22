b66d79261be143ce70f2d12007de9fb38725fbbd
tigerabrodi <tigerabrodi@gmail.com>
2022-11-22 06:59:21 +0100

    feat: implement UI of settings

---
+++ app/icons/Close.tsx
@@ -0,0 +1,10 @@
+export function Close() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 15">
+      <path
+        fill="#075E54"
+        d="M15.1 12.7 9.8 7.4l5.3-5.3L13.3.3 8 5.7 2.7.4.9 2.1l5.3 5.3-5.3 5.3 1.8 1.8L8 9.2l5.3 5.3 1.8-1.8Z"
+      />
+    </svg>
+  )
+}
--- app/routes/chats.$chatId.settings.tsx
+++ app/routes/chats.$chatId.settings.tsx
@@ -1,9 +1,11 @@
 import type { LinksFunction } from '@remix-run/node'
 
 import { Dialog } from '@headlessui/react'
-import { useNavigate } from '@remix-run/react'
+import { Form, Link, useFetcher, useNavigate } from '@remix-run/react'
 
 import styles from './chats.$chatId.settings.css'
+
+import { Close, DefaultChat, Delete, Plus } from '~/icons'
 
 export const links: LinksFunction = () => {
   return [{ rel: 'stylesheet', href: styles }]
@@ -11,8 +13,11 @@ export const links: LinksFunction = () => {
 
 const BACK_ROUTE = '..'
 
+const PARTICIPANT_INPUT_NAME = 'participantId'
+
 export default function Settings() {
   const navigate = useNavigate()
+  const fetcher = useFetcher()
 
   return (
     <Dialog
@@ -21,7 +26,57 @@ export default function Settings() {
       className="settings"
     >
       <Dialog.Panel className="settings__panel">
-        <Dialog.Title className="sr-only">Deactivate account</Dialog.Title>
+        <div className="settings__panel-header">
+          <Link to={BACK_ROUTE} aria-label="Close">
+            <Close />
+          </Link>
+          <Dialog.Title as="h1">Settings</Dialog.Title>
+
+          <Form>
+            <button aria-label="Delete chat">
+              <Delete />
+            </button>
+          </Form>
+        </div>
+
+        <div className="settings__panel-main">
+          <input type="file" className="sr-only" id="image" accept="image/*" />
+
+          <label htmlFor="image">
+            <DefaultChat />
+            <span className="sr-only">Upload image</span>
+          </label>
+
+          <h2>Untitled</h2>
+
+          <p>11 participants</p>
+        </div>
+
+        <div className="settings__panel-participants">
+          <h3>Participants</h3>
+          <Link to="./participants" aria-label="Add new participant">
+            <Plus />
+          </Link>
+
+          <ul>
+            <li>
+              <h4>~ Tiger Abrodi</h4>
+              <p>tigerabrodi@gmail.com</p>
+
+              <fetcher.Form method="post">
+                <input
+                  type="hidden"
+                  name={PARTICIPANT_INPUT_NAME}
+                  value="TODO"
+                />
+
+                <button aria-label="Remove participant CHANGE">
+                  <Close />
+                </button>
+              </fetcher.Form>
+            </li>
+          </ul>
+        </div>
       </Dialog.Panel>
     </Dialog>
   )
