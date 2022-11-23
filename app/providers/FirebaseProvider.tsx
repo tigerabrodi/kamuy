9beb4a8a3d4b56b31640f0ab7c502176aa1183bf
tigerabrodi <tigerabrodi@gmail.com>
2022-11-23 06:15:45 +0100

    feat: impement firebase storage into provider

--- app/providers/FirebaseProvider.tsx
+++ app/providers/FirebaseProvider.tsx
@@ -1,16 +1,19 @@
 import type { FirebaseOptions } from 'firebase/app'
 import type { Firestore } from 'firebase/firestore'
+import type { FirebaseStorage } from 'firebase/storage'
 import type { PropsWithChildren } from 'react'
 
 import { initializeApp } from 'firebase/app'
 import { signInWithCustomToken, getAuth } from 'firebase/auth'
 import { initializeFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
 import { useContext } from 'react'
 import { useEffect } from 'react'
 import { createContext, useState } from 'react'
 
 type FirebaseContextValue = {
   firebaseDb: Firestore | null
+  firebaseStorage: FirebaseStorage | null
 }
 
 const FirebaseContext = createContext<FirebaseContextValue | null>(null)
@@ -28,6 +31,8 @@ export function FirebaseProvider({
 }: FirebaseProviderParams) {
   const [firebaseDb, setFirebaseDb] =
     useState<FirebaseContextValue['firebaseDb']>(null)
+  const [firebaseStorage, setFirebaseStorage] =
+    useState<FirebaseContextValue['firebaseStorage']>(null)
 
   useEffect(() => {
     if (!firebase) {
@@ -37,12 +42,17 @@ export function FirebaseProvider({
     const initFirebaseDb = async () => {
       const app = initializeApp(firebase.options)
       const auth = getAuth(app)
+      const storage = getStorage(app)
 
       if (!auth.currentUser) {
         try {
           await signInWithCustomToken(auth, firebase.userToken)
         } catch (error) {
           console.error('Failed to authenticate user for REAL firestore', error)
+        }
+
+        if (!firebaseStorage) {
+          setFirebaseStorage(storage)
         }
 
         if (!firebaseDb) {
@@ -58,10 +68,10 @@ export function FirebaseProvider({
     initFirebaseDb().catch((error) =>
       console.error('Failed to initialize firebase for REAL firestore', error)
     )
-  }, [firebase, firebaseDb])
+  }, [firebase, firebaseDb, firebaseStorage])
 
   return (
-    <FirebaseContext.Provider value={{ firebaseDb }}>
+    <FirebaseContext.Provider value={{ firebaseDb, firebaseStorage }}>
       {children}
     </FirebaseContext.Provider>
   )
